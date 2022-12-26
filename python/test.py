3685e87f5b1693bcefcdcb622c5288222042a903
MaxenceLearn <maxence.rabrais@gmail.com>
2022-12-26 16:18:54 +0100

    Co-authored-by: Rossamylne <Rossamylne@users.noreply.github.com>

--- python/test.py
+++ python/test.py
@@ -1,3 +1,8 @@
+
+# Algorithme Librairie
+
+
+
 def monnaie_a_rendre(argent):
     '''
     Fonction qui donne la monnaie à rendre en fonction d'un montant donné.
@@ -19,6 +24,73 @@ def monnaie_a_rendre(argent):
                 argent -= monnaie
                 print(f'Il faut rendre {monnaie} euros')
                 break
+'''
+monnaie_a_rendre(1557)
+'''
 
-monnaie_a_rendre(595)
 
+
+# Algorithme du prêt-à-porter
+
+def monnaie_limite(argent):
+    monnaies_limite = {200: 1, 100: 3, 50: 1, 20: 1, 10: 1, 2: 5}
+    while argent > 0:
+        for k in monnaies_limite:
+            if k <= argent:
+                if monnaies_limite.get(k) > 0:
+                    monnaies_limite[k] = monnaies_limite.get(k) - 1
+                    argent -= k
+                    print(f'donner un billet de {k}')
+                    break
+        total = sum(monnaies_limite.values())
+        if total == 0 and argent > 0:
+            print(f'Plus de monnaie, plus aucune monnaie à rendre, vraiment plus de monnaie, désolé ! Revenez plus jamais !')
+            break
+    
+'''       
+monnaie_limite(497)
+'''
+
+
+
+
+
+
+# Algorithme de la baguette magique
+
+'''
+1 GALLION = 17 MORNILLES
+1 MORNILLE = 29 NOISES
+1 GALLION = 493 NOISES
+'''
+
+
+def gallion(mornille):
+    monnaie_gallion = 17
+    if mornille % monnaie_gallion == 0:
+        return int(mornille/monnaie_gallion)
+    else:
+        reste_mornille = mornille % monnaie_gallion
+        return reste_mornille, int(mornille/monnaie_gallion)
+
+def mornille(noise):
+    monnaie_mornille = 29
+    if noise % monnaie_mornille == 0:
+        return int(noise / monnaie_mornille)
+    else:
+        reste_noise = noise % monnaie_mornille
+        return reste_noise, int(noise/monnaie_mornille)
+
+
+
+
+nb_noises = int(input("Nombre de noises : "))
+nb_mornilles = int(input("Nombre de mornilles : "))
+nb_gallion = int(input("Nombre de gallion : "))
+
+'''
+def monnaie_sorcier(noi, mor, gal):
+    return
+'''
+
+print(f'il faut rendre {gallion(nb_mornilles)[1] + nb_gallion} gallions, rendre {gallion(nb_mornilles)[0] + mornille(nb_noises)[1]} mornilles, et rendre {mornille(nb_noises)[0]} noises')
