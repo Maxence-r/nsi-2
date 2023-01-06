b083392eda7d0bbeb4a2bfa33c9978f8d49e540e
Maxence <108428260+MaxenceLearn@users.noreply.github.com>
2023-01-06 23:38:21 +0100

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.download').addEventListener('click', () => {
+    window.location.href = 'https://github.com/MaxenceLearn/nsi-2/'
+})
+
+document.getElementById('github').addEventListener('click', () => {
+    window.location.href = 'https://github.com/MaxenceLearn/nsi-2/blob/main/python/test.py'
+})
